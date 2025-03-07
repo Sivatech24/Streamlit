de19807cc06978c82f37aba5996c6b0dd9efaf6e
codingmaster24 <93309902+Sivatech24@users.noreply.github.com>
2025-03-07 03:31:04 -0800

    Update github.py

--- pages/github.py
+++ pages/github.py
@@ -7,6 +7,6 @@ st.write("You can find the complete source code for this project on GitHub.")
 # Replace with your actual GitHub link
 github_url = "https://github.com/Sivatech24/Streamlit"
 
-st.markdown(f"[📂 View Repository]({github.com/Sivatech24})")
+st.markdown("[📂 View Repository](https://github.com/Sivatech24)")
 
 st.write("If you like this project, consider giving it a ⭐ on GitHub!")
